4ef7ae91a9e6c6430e8342b2caa2117bbabaa444
zhouxin <1470328933@qq.com>
2023-04-26 16:11:37 +0800

    modify

--- test.c
+++ test.c
@@ -1,10 +1,26 @@
 
+
 #include <stdio.h>
-#include <limits.h>
  
 int main()
 {
-   printf("int: %lu \n", sizeof(int));
-   
-   return 0;
+   int a = 21;
+   int b = 10;
+   int c ;
+ 
+   c = a + b;
+   printf("Line 1 - c 的值是 %d\n", c );
+   c = a - b;
+   printf("Line 2 - c 的值是 %d\n", c );
+   c = a * b;
+   printf("Line 3 - c 的值是 %d\n", c );
+   c = a / b;
+   printf("Line 4 - c 的值是 %d\n", c );
+   c = a % b;
+   printf("Line 5 - c 的值是 %d\n", c );
+   c = a++;  // 赋值后再加 1 ，c 为 21，a 为 22
+   printf("Line 6 - c 的值是 %d\n", c );
+   c = a--;  // 赋值后再减 1 ，c 为 22 ，a 为 21
+   printf("Line 7 - c 的值是 %d\n", c );
+ 
 }
